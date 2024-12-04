83b1114c6843befc4f277aa19d60bbb1f79922d4
Mikhail <m.ataev@mail.ru>
2024-12-04 16:34:15 +0100

    Update .gitignore

---
+++ book-crud-app/.angular/cache/19.0.2/book-crud-app/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,37 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-6JUDMY6V.js";
+import "./chunk-EJZH4UR5.js";
+import "./chunk-6WUQZRIK.js";
+import "./chunk-TZH4GKVI.js";
+import "./chunk-Z4UQ4WOW.js";
+import "./chunk-D3SSMTPP.js";
+import "./chunk-4DPES5Z6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
+//# sourceMappingURL=@angular_ssr.js.map
